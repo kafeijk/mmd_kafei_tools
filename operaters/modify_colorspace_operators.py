835668978b68fa9812765b43693b85db2fad366e
kafei <1846807500@qq.com>
2024-05-29 21:52:39 +0800

    feat:增加修改贴图色彩空间功能

---
+++ operaters/modify_colorspace_operators.py
@@ -0,0 +1,78 @@
+from ..utils import *
+
+
+class ModifyColorspaceOperators(bpy.types.Operator):
+    bl_idname = "mmd_kafei_tools.modify_colorspace"
+    bl_label = "修改"
+    bl_description = "修改贴图色彩空间"
+    bl_options = {'REGISTER', 'UNDO'}  # 启用撤销功能
+
+    def execute(self, context):
+        main(self, context)
+        return {'FINISHED'}  # 让Blender知道操作已成功完成
+
+
+def check_props(operator, props):
+    colorspace = props.colorspace
+    keywords = props.keywords
+    selected_only = props.selected_only
+
+    if not colorspace:
+        operator.report(type={'ERROR'}, message=f'请选择色彩空间！')
+        return False
+    if not keywords and not selected_only:
+        operator.report(type={'ERROR'}, message=f'未填写关键词时，必须开启仅选中！')
+        return False
+    if selected_only and len(bpy.context.selected_objects) == 0:
+        operator.report(type={'ERROR'}, message=f'请选择至少一个物体！')
+        return False
+    return True
+
+
+def main(operator, context):
+    scene = context.scene
+    props = scene.mmd_kafei_tools_modify_colorspace
+    check_props(operator, props)
+    colorspace = props.colorspace
+    keywords = props.keywords
+    selected_only = props.selected_only
+    objs = bpy.context.selected_objects
+    # 关键词列表
+    keyword_list = []
+    if keywords:
+        keyword_list = [keyword.lower().strip() for keyword in keywords.split(",")]
+
+    # 如果作用于全部物体，则直接对图像对象进行修改
+    if not selected_only:
+        for image in bpy.data.images:
+            if any(keyword in image.name.lower().strip() for keyword in keyword_list):
+                # 修改图像的色彩空间
+                image.colorspace_settings.name = colorspace
+    else:
+        # 如果不是作用于全部物体，则需要筛选出作用的图像对象
+        image_names = set()
+        for obj in objs:
+            if not obj.material_slots:  # 材质槽为空
+                continue
+            for slot in obj.material_slots:
+                material = slot.material
+                if not material:  # 有材质槽但无材质
+                    continue
+
+                node_tree = material.node_tree
+                if not node_tree:  # 有材质但无节点树
+                    continue
+
+                nodes = node_tree.nodes
+                if not nodes:  # 有节点树但无节点
+                    continue
+
+                for node in nodes:
+                    if node.type == 'TEX_IMAGE':
+                        image = node.image
+                        if len(keyword_list) == 0:
+                            image.colorspace_settings.name = colorspace
+                        else:
+                            if any(keyword in image.name.lower().strip() for keyword in keyword_list):
+                                # 修改图像的色彩空间
+                                image.colorspace_settings.name = colorspace
