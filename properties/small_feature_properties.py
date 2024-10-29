e0d2509ca5f417da96a61238940d4b21d73687b0
kafei <1846807500@qq.com>
2024-10-29 08:39:55 +0800

    fix:修改选择烘焙骨骼的相关描述

--- properties/small_feature_properties.py
+++ properties/small_feature_properties.py
@@ -7,7 +7,7 @@ class SmallFeatureProperty(bpy.types.PropertyGroup):
         description="用途",
         items=[
             ("SCENE_ROOT", "创建场景控制器", "创建一个空物体，以实现对整个场景的统一控制"),
-            ("BAKE", "选择MMD常用骨骼", "选择MMD常用骨骼，以便烘焙VMD动作")
+            ("BAKE", "选择烘焙动作骨骼", "选择用于烘焙VMD动作的MMD骨骼")
         ],
         default="SCENE_ROOT"
     )
