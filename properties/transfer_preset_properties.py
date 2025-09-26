fdb905ba3b8cd85a7f664a07fa02ee4670592f1d
kafei <1846807500@qq.com>
2025-09-27 01:32:15 +0800

    feat:优化预设传递逻辑，优化预设传递提示

--- properties/transfer_preset_properties.py
+++ properties/transfer_preset_properties.py
@@ -23,7 +23,17 @@ def auto_fill(self, context):
 
 def update_preset(self, context):
     if self.direction == 'PMX2PMX':
+        self.material_flag = True
+        self.uv_flag = False
+        self.vgs_flag = True
+        self.modifiers_flag = True
         self.toon_shading_flag = False
+    elif self.direction == 'PMX2ABC':
+        self.material_flag = True
+        self.uv_flag = True
+        self.vgs_flag = True
+        self.modifiers_flag = True
+        self.normal_flag = True
 
 
 class TransferPresetProperty(bpy.types.PropertyGroup):
