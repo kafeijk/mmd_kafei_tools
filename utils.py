fd2f8b1dfc967c21de84a96b635951541e1ae0d2
kafei <1846807500@qq.com>
2026-03-04 01:58:57 +0800

    fix:修复批量预处理时，无法识别Pmx等后缀的情况

--- utils.py
+++ utils.py
@@ -236,13 +236,13 @@ def recursive_search(directory, suffix, threshold, search_strategy, conflict_str
         flag = False
 
         for file in files:
-            if file.endswith('.pmx') or file.endswith('.pmd'):
+            if file.lower().endswith(('.pmx', '.pmd')):
                 flag = True
                 pmx_count += 1
         if flag:
             curr_list = []  # 当前目录下符合条件的文件
             model_files = [f for f in files
-                           if (f.endswith('.pmx') or f.endswith('.pmd'))
+                           if f.lower().endswith(('.pmx', '.pmd'))
                            and os.path.getsize(os.path.join(root, f)) > threshold * 1024]  # 排除掉已被排除的文件的影响
 
             # 如果满足条件的model_files有多个，取最新的还是取全部
@@ -291,12 +291,12 @@ def recursive_search_img(directory, suffix, threshold, search_strategy, conflict
     for root, dirs, files in os.walk(directory):
         flag = False
         for file in files:
-            if file.endswith('.pmx') or file.endswith('.pmd'):
+            if file.lower().endswith(('.pmx', '.pmd')):
                 flag = True
                 pmx_count += 1
         if flag:
             model_files = [f for f in files
-                           if (f.endswith('.pmx') or f.endswith('.pmd'))
+                           if f.lower().endswith(('.pmx', '.pmd'))
                            and os.path.getsize(os.path.join(root, f)) > threshold * 1024]  # 排除掉已被排除的文件的影响
 
             # 如果满足条件的model_files有多个，取最新的还是取全部
@@ -504,7 +504,7 @@ def batch_process(func, props, f_flag=False):
     for index, filepath in enumerate(file_list):
         get_collection(TMP_COLLECTION_NAME)
         file_base_name = os.path.basename(filepath)
-        ext = os.path.splitext(filepath)[1]
+        ext = os.path.splitext(filepath)[1].lower()
         if ".pmd" == ext:
             ext = ".pmx"  # 再导出的时候是pmx格式的，如果依然以pmd为后缀，导入PE会报错
 
